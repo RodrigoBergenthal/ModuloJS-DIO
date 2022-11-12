5406ce2184e581c44ba55dab61905326e75bf35e
RodrigoBergenthal <rodrigo_bergenthal@live.com>
2022-11-11 21:10:55 -0300

    atualizando

--- 04- funcoes/aula3.js
+++ 04- funcoes/aula3.js
@@ -1,6 +1,7 @@
 
-function sayMyName(name) {
-    console.log('Your name is:' + name);
+function incrementarJuros(valor, percentualJuros) {
+    const valorDeAcrescimo = (percentualJuros / 100) * valor;
+    return valor + valorDeAcrescimo;
 }
 
-sayMyName('Rodrigo');
+console.log(incrementarJuros(100, 10));
