de38161da494c2f5b88c2224d0b1026da6250565
RodrigoBergenthal <rodrigo_bergenthal@live.com>
2022-11-14 08:02:57 -0300

    atualizando

---
+++ 04- funcoes/aula04.js
@@ -0,0 +1,14 @@
+function escrevaMeuNome(nome) {
+    console.log('Meu nome é ' + nome);
+}
+escrevaMeuNome('Rodrigo');
+
+function verificarIdade(idade) {
+    if (idade >= 18){
+    console.log('Maior de idade')
+} else {
+    console.log('Menor de idade')
+    }
+}
+verificarIdade(18)   
+
