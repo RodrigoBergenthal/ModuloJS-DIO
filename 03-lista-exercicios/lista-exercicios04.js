1273c878647023ad7eaa8525b7d16db070a9fd37
RodrigoBergenthal <rodrigo_bergenthal@live.com>
2022-11-14 23:14:07 -0300

    atualizando

--- 03-lista-exercicios/lista-exercicios04.js
+++ 03-lista-exercicios/lista-exercicios04.js
@@ -1,16 +1,16 @@
-/*
-1- crie uma classe para representar carros.
-Os carros possuem uma marca, uma cor e um gasto médio de combustivel por kilometro rodado.
-Crie um metodo que dado a quantidade de quilometros e o preço do combustivel nos de o valor gasto em reais para realizar este percurso.
-*/
-
 class carros {
     marca;
     cor;
     gastoKM;
+    constructor(marca,cor,gastoKM){
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoKM = gastoKM;
+    }
+        
 }
 
-const corsa = new carros('Corsa', 'preto', 15);
-const palio = new carros('palio', 'branco', 20);
+const corsa = new carros('Corsa', 'preto', 1/15);
+const palio = new carros('palio', 'branco', 1/20);
 
-console.log(corsa)
+console.log(corsa);
